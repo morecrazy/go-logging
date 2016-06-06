239a8f53514b2e982e16650e3d5c1025ced839d7
liudanking <liudanking@gmail.com>
2016-06-06 11:27:22 +0800

    update sentry logger

--- sentry.go
+++ sentry.go
@@ -1,6 +1,9 @@
 package logging
 
-import "third/raven-go"
+import (
+	"fmt"
+	"third/raven-go"
+)
 
 type SentryBackend struct {
 	client *raven.Client
@@ -19,15 +22,15 @@ func (b *SentryBackend) Log(level Level, calldepth int, rec *Record) error {
 	line := rec.Formatted(calldepth + 1)
 
 	if level <= b.level {
-		//var in_err error
+		var in_err error
 		packet := raven.NewPacket(line, trace())
 		b.client.Capture(packet, nil)
-		//eventID, ch := b.client.Capture(packet, nil)
-		//		//不判断ch 可提高效率，但会发送不成功
-		//		if in_err = <-ch; in_err != nil {
-		//			message := fmt.Sprintf("Error event with id %s,%v", eventID, in_err)
-		//			fmt.Println(message)
-		//		}
+		eventID, ch := b.client.Capture(packet, nil)
+		//不判断ch 可提高效率，但会发送不成功
+		if in_err = <-ch; in_err != nil {
+			message := fmt.Sprintf("Error event with id %s,%v", eventID, in_err)
+			fmt.Println(message)
+		}
 	}
 
 	return nil
